05be6d975bafa592734cca571d97b03a35e926e2
Domingos Machado <d.machado1993@gmail.com>
2025-05-20 15:21:19 -0300

    melhorias

--- src/main/java/org/serratec/avaliacao/controller/FuncionarioController.java
+++ src/main/java/org/serratec/avaliacao/controller/FuncionarioController.java
@@ -19,17 +19,20 @@ public class FuncionarioController {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    // GET para listar TODOS os funcionários
     @GetMapping
     public ResponseEntity<List<Funcionario>> listarTodos() {
         return ResponseEntity.ok(funcionarioRepository.findAll());
     }
 
+    // GET para listar um funcionário(ID) específico
     @GetMapping("/{id}")
     public ResponseEntity<Funcionario> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(funcionarioRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("ID inexistente" + id)));
     }
 
+    // POST para inserir um funcionário(ID)
     @PostMapping
     public ResponseEntity<Funcionario> inserir(@Valid @RequestBody Funcionario funcionario) {
         funcionario.setId(null);
@@ -37,6 +40,7 @@ public class FuncionarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(novoFuncionario);
     }
 
+    // PUT para atualizar um funcionário(ID)
     @PutMapping("/{id}")
     public ResponseEntity<Funcionario> atualizar(@PathVariable Long id, @Valid @RequestBody Funcionario funcionario) {
         if (!funcionarioRepository.existsById(id)) {
@@ -48,6 +52,7 @@ public class FuncionarioController {
         return ResponseEntity.ok(funcionarioAtualizado);
     }
 
+    // DELETE para remover um funcionário(ID)
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> remover(@PathVariable Long id) {
         if (!funcionarioRepository.existsById(id)) {
--- src/main/java/org/serratec/avaliacao/model/Funcionario.java
+++ src/main/java/org/serratec/avaliacao/model/Funcionario.java
@@ -3,7 +3,6 @@ package org.serratec.avaliacao.model;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,7 +23,8 @@ public class Funcionario {
     @Size(min = 3, max = 50, message = "Nome deve ter entre 3 e 50 caracteres")
     private String nome;
 
-    @NotNull(message = "Endereço é obrigatório")
+
     @Valid
+    @Embedded
     private Endereco endereco;
 }
